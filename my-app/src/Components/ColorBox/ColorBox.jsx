2b660fb8dddb388820f3035bf3b19d293771bb6e
Mahen Paraameshwar <mahe365param@gmail.com>
2020-12-20 09:33:43 +0530

    copy functionality overlay done

--- my-app/src/Components/ColorBox/ColorBox.jsx
+++ my-app/src/Components/ColorBox/ColorBox.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../../Styles/colorbox.css'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
 
 function ColorBox({name,color}) {
+    const [isCopied,setIsCopied] = useState(false);
+    
+    const timer = useRef();
+    
+    const handelDisplayOverlay=()=>{
+        setIsCopied(true);
+        timer.current = setTimeout(()=>{
+            setIsCopied(false);
+        },1500)
+    }
+
+    /*Handels display of overlay */
+    useEffect(()=>{
+    
+    return ()=>{
+        if(isCopied){
+            clearInterval(timer.current);
+        }
+    }
+    },[isCopied])
+
+
 
     return (
-        <div style={{backgroundColor:color}} className="color__box">
-            <div className="box__container">
-                <div className="color__name">
-                    {name}
+        <>
+        <CopyToClipboard text={color} onCopy={handelDisplayOverlay}>
+            <div style={{backgroundColor:color}} className="color__box">
+                <div style={{backgroundColor:color}} className={`overlay ${isCopied && 'show' }`}/>
+                <div className="box__container">
+                    <div className="color__name">
+                        {name}
+                    </div>
+                    <div className="see__more">
+                        More
+                    </div>
                 </div>
-                <div className="see__more">
-                    More
-                </div>
+                <button className = "copy__btn">
+                        Copy
+                </button>
             </div>
-            <button className = "copy__btn">
-                    Copy
-            </button>
+        </CopyToClipboard>
+        <div className={`copy__msg ${isCopied && 'show' }`}>
+            <h1>copied!</h1>
+            <p>{color}</p>
         </div>
+        </>
     );
 }
 
